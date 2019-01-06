dfa245b1b6d7454878430c36e07193c57b215f60
veely1 <veely1@gmail.com>
2019-01-06 04:15:26 -0500

    added controllable player object

--- public/assets/game.mjs
+++ public/assets/game.mjs
@@ -1,26 +1,27 @@
-import { Ball } from './gameObjects.mjs';
+import { Ball, Player } from './gameObjects.mjs';
 
 window.onload = function() {
   const canvas = document.getElementById("gameBoard");
   const ctx = canvas.getContext("2d");
   
-  let x = 50;
-  let y = 50;
-  let dx = 2;
-  let dy = 2;
-  let ballRadius = 10;
-  
   let rightPressed = false;
   let leftPressed = false;
-  let upPressed = false;
-  let downPressed = false;
+  // let upPressed = false;
+  // let downPressed = false;
   
   var ball1 = new Ball(50, 50, 10);
-
+  var player = new Player(2, 3, canvas);
 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ball1.drawBall(ctx, canvas);
+    player.drawPaddle(ctx, canvas);
+    if (rightPressed) {
+      player.moveRight();
+    }
+    if (leftPressed) {
+      player.moveLeft();
+    }
   }
   
   document.addEventListener("keydown", keyDownHandler, false);
